f16c7c60a1a08c9e5ea12684aa0e45c5e32268f6
Lwist-wei <2046018311@qq.com>
2021-08-24 21:44:17 +0800

    '修改'

--- src/network/home.js
+++ src/network/home.js
@@ -1,6 +1,17 @@
 import {request} from "./request";
+import axios from "../../../HYMall-master/HYMall-master/src/network/axios";
 export function getHomeMultidata(){
   return request({
     url:'/home/multidata'
+  })
+}
+
+export function getHomeData(type, page) {
+  return axios({
+    url: '/home/data',
+    params: {
+      type,
+      page
+    }
   })
 }
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,6 +6,11 @@
     <home-swiper :banners="banners"></home-swiper>
     <home-recommend-view :recommends="recommends"></home-recommend-view>
     <feature-view></feature-view>
+    <tab-control class="tab-control"
+      :titles="['流行','新款','精选']"
+      @tabClick="tabClick"
+    ></tab-control>
+    <goods-list :goods="goodsList.pop.list"></goods-list>
     <ul>
       <li>利弊</li>
       <li>利弊</li>
@@ -113,31 +118,61 @@
 </template>
 
 <script>
-import {getHomeMultidata} from "network/home";
+import {getHomeMultidata,getHomeData} from "network/home";
 import NavBar from "components/common/navbar/Navbar";
 import HomeSwiper from "./Childcomps/HomeSwiper";
 import HomeRecommendView from "./Childcomps/HomeRecommendView";
 import FeatureView from "./Childcomps/FeatureView";
 
+import TabControl from "components/content/TabControl/TabControl";
+import GoodsList from "components/content/goods/GoodsList";
 export default {
   name: "Home",
   components: {
     NavBar,
     HomeSwiper,
     HomeRecommendView,
-    FeatureView
+    FeatureView,
+    TabControl,
+    GoodsList
   },
   data() {
     return {
       banners: [],
-      recommends: []
+      recommends: [],
+      goodsList: {
+        'pop': {page: 0, list: []},
+        'new': {page: 0, list: []},
+        'sell':{page: 0, list: []}
+      },
     }
   },
   created() {
-    getHomeMultidata().then(res => {
+    this.getMultidata();
+    this.getHomeProducts('pop',this.page);
+    this.getHomeProducts('new',this.page);
+    this.getHomeProducts('sell',this.page);
+  },
+  methods:{
+    // 事件监听
+    tabClick(index){
+
+    },
+
+
+
+    getMultidata(){
+      getHomeMultidata().then(res => {
       this.recommends = res.data.recommend.list;
       this.banners = res.data.banner.list;
-    })
+    })},
+    getHomeProducts(type) {
+      let page=this.goodsList[type].page+1
+      getHomeData(type,page).then(res=>{
+       this.goodsList[type].list.push(...res.data.list)
+      })
+      this.goodsList[type].page+=1;
+    }
   }
 }
 </script>
